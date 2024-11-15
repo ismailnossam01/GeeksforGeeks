c1c261150abddbe5d696df203689f0ff82458f3e
ismail_nossam_01 <138509028+ismailnossam01@users.noreply.github.com>
2024-11-15 22:09:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Second Largest/second-largest.py
@@ -0,0 +1,24 @@
+#User function Template for python3
+class Solution:
+    def getSecondLargest(self, arr):
+        a=list(set(arr))
+        a.sort()
+        if len(a)<=1:
+            return -1
+        else:
+            return a[-2]
+
+
+#{ 
+ # Driver Code Starts
+# Initial Template for Python 3
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+        arr = list(map(int, input().split()))
+        ob = Solution()
+        ans = ob.getSecondLargest(arr)
+        print(ans)
+        print("~")
+# } Driver Code Ends
